3487fcdb856e72a7767962d9e586b78d154d5a20
Joseph Guhlin <joseph.guhlin@gmail.com>
2013-03-12 12:40:55 -0500

    we need a github for svn. git just isn't up to spec

--- bin/biotools/blast.clj
+++ bin/biotools/blast.clj
@@ -1,4 +1,8 @@
-(ns biotools.blast)
+(ns biotools.blast
+    (:require clojure.java.io
+            clojure.string
+            [clojure.core.reducers :as r]
+            [iota :as iota]))
 
 (defrecord BlastHit [query-id subject-id pct-identity alignment-length mismatches gap-opens query-start query-end subject-start subject-end evalue bitscore query-length subject-length query-alignment-percent])
 
@@ -27,11 +31,32 @@
 (defn parse-reader 
   "Parse BLAST+ results from a reader. Should be lazy. Now accepts minimum %ID and minimum alignment length as a percentage.
    Percent alignment is based off of query length to the alignment-length, as a percentage. A 211nt alignment where 211 align
-   from the blast results are 100% aligned. Keep best option is in the translator/database part."
+   from the blast results are 100% aligned."
   ([opts rdr]
   (for [entry (line-seq rdr)]
     (-parse opts entry)))
   ([opts pct-id-min pct-align-min rdr]
-    (keep (fn [x] (if (and (>= (:pct-identity x) (Float/parseFloat pct-id-min)) (>= (:query-alignment-percent x) (Float/parseFloat pct-align-min))) x nil)) 
+    (keep (fn [x] (if (and (>= (:pct-identity x) pct-id-min) (>= (:query-alignment-percent x) pct-align-min)) x nil)) 
                     (for [entry (line-seq rdr)]
                       (-parse entry)))))
+
+(defn -create-filter-fn 
+  [pct-id-min pct-align-min]
+  (fn [x]
+    (if
+      (and
+        (>= (:pct-identity x) pct-id-min)
+        (>= (:query-alignment-percent x) pct-align-min))
+      x
+      nil)))
+
+(defn parse-reader-via-reducer
+  "Parse BLAST+ results from a filename. Not lazy, but can exceed memory. Also accepts minimum %ID and minimum alignment length as a percentage.
+   Percent alignment is based off of query length to the alignment-length, as a percentage. A 211nt alignment where 211 align
+   from the blast results are 100% aligned."
+  ([opts pct-id-min pct-align-min filename]
+    (let [filter-fn (-create-filter-fn pct-id-min pct-align-min)]
+      (->> (iota/vec filename)
+        (r/map -parse)
+        (r/filter filter-fn)
+        (r/foldcat)))))
